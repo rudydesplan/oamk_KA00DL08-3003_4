0c27215181c25b44923d094d181d53c48ac46ec9
rudy desplan <desrudy@hotmail.com>
2025-03-20 01:02:09 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+import streamlit as st
+
+st.set_page_config(
+    page_title="Housing Price Analysis",
+    page_icon="🏠",
+    layout="wide"
+)
+
+st.title("Ames Housing Price Prediction Analysis")
+st.markdown("Navigate through pages using the sidebar 👈")
